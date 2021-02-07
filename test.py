4025932d270c82481b50a82451230bd06f30d449
shavaun1 <jshavaun@yahoo.com>
2021-02-06 21:24:02 -0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-
-print("Enter the word you are looking for: ")
-x = input()
-K = open('The_Gray_Nun.txt','r')
-rd = K.read()
-oc = rd.count(x)
-print("Number of occurances of the word-and: ", oc)
-
