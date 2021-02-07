19fce53c6e1c14459235c2172e076f7a7c841fd4
shavaun1 <jshavaun@yahoo.com>
2021-02-06 21:24:41 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+
+print("Enter the word you are looking for: ")
+x = input()
+K = open('The_Gray_Nun.txt','r')
+rd = K.read()
+oc = rd.count(x)
+print("Number of occurances of the word-and: ", oc)
+K.close()
+
